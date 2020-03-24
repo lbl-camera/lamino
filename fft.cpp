542bc9b60c44b1cc294fd9f94ec792b9a23e00c3
Dinesh Kumar <dkumar@lbl.gov>
2020-03-23 17:17:44 -0700

    cleaned up.

--- fft.cpp
+++ fft.cpp
@@ -8,7 +8,10 @@
 #include "fft.h"
 
 
-void fft1d(fftw_complex * input, dim3_t dims) {
+void fft1(Array<complex_t> &input) {
+
+    dim3_t dims = input.dims();
+    fftwf_complex * data = (fftwf_complex *) input.ptr();
     int rank = 1;
     int n[] = { dims.z };
     int idist = dims.z;
@@ -18,13 +21,33 @@ void fft1d(fftw_complex * input, dim3_t dims) {
     int *inembed = NULL;
     int *onembed = NULL;
     int batches = dims.x * dims.y;
-    fftw_plan plan = fftw_plan_many_dft(rank, n, batches, input, inembed,
-        istride, idist, input, onembed, ostride, odist, FFTW_FORWARD, FFTW_ESTIMATE);
-    fftw_execute(plan);
-    fftw_destroy_plan(plan);
+    fftwf_plan plan = fftwf_plan_many_dft(rank, n, batches, data, inembed,
+        istride, idist, data, onembed, ostride, odist, FFTW_FORWARD, FFTW_ESTIMATE);
+    fftwf_execute(plan);
+    fftwf_destroy_plan(plan);
 }
+void ifft1(Array<complex_t> &input) {
 
-void ifft2d(fftw_complex * input, dim3_t dims) {
+    dim3_t dims = input.dims();
+    fftwf_complex * data = (fftwf_complex *) input.ptr();
+    int rank = 1;
+    int n[] = { dims.z };
+    int idist = dims.z;
+    int odist = dims.z;
+    int istride = 1;
+    int ostride = 1;
+    int *inembed = NULL;
+    int *onembed = NULL;
+    int batches = dims.x * dims.y;
+    fftwf_plan plan = fftwf_plan_many_dft(rank, n, batches, data, inembed,
+        istride, idist, data, onembed, ostride, odist, FFTW_BACKWARD, FFTW_ESTIMATE);
+    fftwf_execute(plan);
+    fftwf_destroy_plan(plan);
+}
+void fft2(Array<complex_t> &input) {
+
+    dim3_t dims = input.dims();
+    fftwf_complex * data = (fftwf_complex *) input.ptr();
     int rank = 2;
     int n[] = { dims.y, dims.z };
     int idist = dims.y * dims.z;
@@ -34,44 +57,44 @@ void ifft2d(fftw_complex * input, dim3_t dims) {
     int *inembed = NULL;
     int *onembed = NULL;
     int batches = dims.x;
-    fftw_plan plan = fftw_plan_many_dft(rank, n, batches, input, inembed,
-        istride, idist, input, onembed, ostride, odist, FFTW_BACKWARD, FFTW_ESTIMATE);
-    fftw_execute(plan);
-    fftw_destroy_plan(plan);
+    fftwf_plan plan = fftwf_plan_many_dft(rank, n, batches, data, inembed,
+        istride, idist, data, onembed, ostride, odist, FFTW_FORWARD, FFTW_ESTIMATE);
+    fftwf_execute(plan);
+    fftwf_destroy_plan(plan);
+}
+void ifft2(Array<complex_t> &input) {
+
+    dim3_t dims = input.dims();
+    fftwf_complex * data = (fftwf_complex *) input.ptr();
+    int rank = 2;
+    int n[] = { dims.y, dims.z };
+    int idist = dims.y * dims.z;
+    int odist = dims.y * dims.z;
+    int istride = 1;
+    int ostride = 1;
+    int *inembed = NULL;
+    int *onembed = NULL;
+    int batches = dims.x;
+    fftwf_plan plan = fftwf_plan_many_dft(rank, n, batches, data, inembed,
+        istride, idist, data, onembed, ostride, odist, FFTW_BACKWARD, FFTW_ESTIMATE);
+    fftwf_execute(plan);
+    fftwf_destroy_plan(plan);
 }
 
-void fftshift1d(complex_t * input, dim3_t dims) {
-    int batches = dims.x * dims.y;
-
-    #pragma omp parallel for
-    for (int i = 0; i< batches; i++)
-        for (int j = 0; j < dims.z; j++) {
-            double a = std::pow(-1, j & 1);
-            int id = i * dims.z + j;
-            input[id] *= a;
-        }
+void fftshift1(Array<complex_t> &input) {
+    dim3_t dims = input.dims();
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i< dims.x; i++)
+        for (int j = 0; j < dims.y; j++) 
+                for (int k = 0; k < dims.z; k++)
+                    input(i,j,k) *= std::pow(-1, k & 1);
 }
 
-void fftshift2d(complex_t * input, dim3_t dims) {
-    int ny = dims.y;
-    int nz = dims.z;
-    #pragma omp parallel for
-    for (int j = 0; j < ny; j++) 
-        for (int k = 0; k < nz; k++) {
-            double a = std::pow(-1, (j+k)&1);
-            int id = j * nz + k;
-            input[id] *= a;
-        }
-}
-
-void xfftshift(complex_t * input, dim3_t dims, double shift) {
-    int batches = dims.x * dims.y;
-    #pragma omp parallel for
-    for (int i = 0; i< batches; i++)
-        for (int j = 0; j < dims.z; j++) {
-            double d = 2 * M_PI * shift * j /((double) dims.z);
-            int id = i * dims.z + j;
-            complex_t I1 = complex_t(0, 1);
-            input[id] = input[id] * exp(-I1 * d);
-        }
+void fftshift2(Array<complex_t> &input) {
+    dim3_t dims = input.dims();
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < dims.x; i++)
+        for (int j = 0; j < dims.y; j++)
+            for (int k = 0; k < dims.z; k++)
+                input(i,j,k) *= std::pow(-1, (j + k) & 1);
 }
--- fft.h
+++ fft.h
@@ -5,17 +5,14 @@
 #include <fftw3.h>
 
 #include "dtypes.h"
+#include "array.h"
 
+void fft1(Array<complex_t> &);
+void ifft1(Array<complex_t> &);
+void fft2(Array<complex_t> &);
+void ifft2(Array<complex_t> &);
 
-void fft1d(fftw_complex *, dim3_t);
-
-void ifft2d(fftw_complex *, dim3_t);
-
-void fftshift1d(complex_t *, dim3_t);
-
-void fftshift2d(complex_t *, dim3_t);
-
-void xfftshift(complex_t *, dim3_t, double);
-
+void fftshift1(Array<complex_t> &);
+void fftshift2(Array<complex_t> &);
 
 #endif // FFTDEFS__H
