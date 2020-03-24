8a9e081029441cef843b4b63ba830b97f10659e6
Dinesh Kumar <dkumar@lbl.gov>
2020-03-23 17:16:48 -0700

    cleaned up. removed unsed parts

--- dtypes.h
+++ dtypes.h
@@ -8,16 +8,9 @@ struct dim3_t {
     int x;
     int y;
     int z;
-    dim3_t(int a, int b, int c): x(a), y(b), z(c) {}
 };
 
-template<class T>
-struct coord_t {
-    T x;
-    T y;
-};
-
-typedef std::complex<double> complex_t;
+typedef std::complex<float> complex_t;
 
 
 #endif // DTYPES__H
