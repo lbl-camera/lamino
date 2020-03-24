091538fb9ea152b5e23aa6d06d2564e701010a1f
Dinesh Kumar <dkumar@lbl.gov>
2020-03-23 17:14:32 -0700

    1. Added NUFFT in both directions. 2. Moved weights to different file.

--- convolve.cpp
+++ convolve.cpp
@@ -1,61 +1,79 @@
 
 
-#include <cmath>
-#include <tr1/cmath>
 #include <iostream>
 
+#include "array.h"
 #include "dtypes.h"
+#include "kernel.h"
 
-const double W = 5;
-const double beta = 3 * M_PI;
+void convolve_p2c(Array_ref<complex_t> input, Array_ref<complex_t> output, Array_ref<float> angles, Kernel kernel) {
 
-double kaiser(double r) {
-    double RMAX = 0.5 * (W - 1);
-    double weight = 0.;
-    if (std::abs(r) <  RMAX) {
-        double t0 = 2 * r / W;
-        double t1 = std::tr1::cyl_bessel_i(0, beta * std::sqrt(1 - t0 * t0));
-        double t2 = std::tr1::cyl_bessel_i(0, beta);
-        weight = t1/t2;
-    }
-    return weight;
+    dim3_t idims = input.dims();
+    dim3_t odims = output.dims();
+    float center = (float) (idims.z / 2);
+
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < idims.x; i++) {
+        for (int j = 0; j < idims.y; j++) {
+            float angle = angles[j];
+            float cosa = std::cos(angle);
+            float sina = std::sin(angle);
+
+            for (int k = 0; k < idims.z; k++){
+                float y = (k - center) * sina + center;
+                float z = (k - center) * cosa + center;
+                complex_t val = input(i, j, k);
+
+                float r = kernel.radius();
+                int iy = std::max((int) (y - r), 0);
+                int iymax = std::min((int) (y + r), odims.y-1);
+                int izmin = std::max((int) (z - r), 0);
+                int izmax = std::min((int) (z + r), odims.z-1);
+ 
+                for (; iy < iymax; iy++) {
+                    complex_t temp = val * kernel.weight(y - iy);
+                    for (int iz = izmin; iz < izmax; iz++) {
+                        #pragma omp critical
+                        output(i, iy, iz) += temp * kernel.weight(z - iz);
+                    } // iz
+                } // iy
+            } // k
+        } // j
+    } // i
 }
 
-void convolve(complex_t *input, complex_t *output, dim3_t idims, dim3_t odims, float *angles) {
+void convolve_c2p(Array_ref<complex_t> input, Array_ref<complex_t> output, Array_ref<float> angles, Kernel kernel) {
 
-    double center = (double) (idims.z / 2);
+    dim3_t idims = input.dims();
+    dim3_t odims = output.dims();
+    float center = (float) (idims.z / 2);
 
-    #pragma omp parallel for
-    for (int j = 0; j < idims.y; j++) {
-        double angle = angles[j];
-        double cosa = std::cos(angle);
-        double sina = std::sin(angle);
-        for (int k = 0; k < idims.z; k++){
-            // global id (input)
-            int ii = j * idims.z + k;
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < idims.x; i++) {
+        for (int j = 0; j < idims.y; j++) {
+            float angle = angles[j];
+            float cosa = std::cos(angle);
+            float sina = std::sin(angle);
 
-            double x = (k - center) * cosa + center;
-            double y = (k - center) * sina + center;
-            complex_t val = input[ii];
+            for (int k = 0; k < idims.z; k++){
+                float y = (k - center) * sina + center;
+                float z = (k - center) * cosa + center;
 
-            int iy = std::max((int) y - 2, 0);
-            int ixmin = std::max((int) x - 2, 0);
-            int iymax = std::min((int) y + 3, odims.y-1);
-            int ixmax = std::min((int) x + 3, odims.z-1);
+                float r = kernel.radius();
+                int iy = std::max((int) (y - r), 0);
+                int iymax = std::min((int) (y + r), odims.y-1);
+                int izmin = std::max((int) (z - r), 0);
+                int izmax = std::min((int) (z + r), odims.z-1);
  
-            for (; iy < iymax; iy++) {
-                double wy = kaiser(y - iy);
-                if (wy < 1.0E-10) continue;
-                complex_t temp = val * wy;
-                for (int ix = ixmin; ix < ixmax; ix++) {
-                    double wx = kaiser(x - ix);
-                    if (wx < 1.0E-10) continue;
-                    // global id (output)
-                    int jj = iy * odims.z + ix;
-                    #pragma omp critical
-                    output[jj] += temp * wx;
-                }
-            }
-        }
-    }
+                for (; iy < iymax; iy++) {
+                    float wy = kernel.weight(y - iy);
+                    for (int iz = izmin; iz < izmax; iz++) {
+                        float wz = kernel.weight(z - iz);
+                        #pragma omp critical
+                        output(i, j, k) += input(i, iy, iz) * wy * wz;
+                    } // ix
+                } // iy
+            } // k
+        } // j
+    } // i
 }
