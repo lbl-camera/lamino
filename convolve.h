480dccff6f16cfdeaf2df7b8fd10942c7147b7c6
Dinesh Kumar <dkumar@lbl.gov>
2020-03-23 17:15:09 -0700

    NUFFT related function declarations

--- convolve.h
+++ convolve.h
@@ -1,13 +1,21 @@
 
 
 #include "dtypes.h"
-
+#include "array.h"
+#include "kernel.h"
 
 #ifndef CONVOLVE__H
 #define CONVOLVE__H
 
-void deapod(complex_t *, dim3_t);
-void convolve(complex_t *, complex_t *, dim3_t , dim3_t, float *);
 
+void convolve_p2c(Array_ref<complex_t>, Array_ref<complex_t>, Array_ref<float>, Kernel);
+void convolve_c2p(Array_ref<complex_t>, Array_ref<complex_t>, Array_ref<float>, Kernel);
+void deconvolve(Array_ref<complex_t>, Kernel);
+
+void forward(Array_ref<complex_t>, Array_ref<complex_t>, Array_ref<float>, Kernel);
+void backward(Array_ref<complex_t>, Array_ref<complex_t>, Array_ref<float>, Kernel);
+
+double calc_error(float *, Array_ref<complex_t>, int);
+void write_output(Array_ref<complex_t>, int);
 
 #endif // CONVOLVE__H
