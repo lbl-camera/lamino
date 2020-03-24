14961a90b4f0dc12bac2bdabd37e4d5d92a8e54f
Dinesh Kumar <dkumar@lbl.gov>
2020-03-23 17:15:36 -0700

    cleaned up a bit

--- deapod.cpp
+++ deapod.cpp
@@ -3,34 +3,30 @@
 #include <iostream>
 
 #include "dtypes.h"
+#include "array.h"
+#include "kernel.h"
 
-double kaiserFT(double W, double beta, double x) {
-    double w = 1;
-    double t1 = (beta * beta) - (x * x * W * W * M_PI * M_PI);
-    if (t1 > 0) {
-        t1 = std::sqrt(t1);
-        double t2 = W / std::tr1::cyl_bessel_i(0, beta);
-        w = t2 * std::sinh(t1)/t1;
-    }
-    return w;
-} 
+void deconvolve(Array<complex_t> & arr, Kernel kernel) {
 
+    dim3_t dims = arr.dims();
 
-void deapod(complex_t *arr, dim3_t dims) {
+    // bit hacky, but wtf
+    bool twod = false;
+    if (dims.y == dims.z) 
+        twod = true;
 
-    double W = 5.;
-    double beta = 12.5663706144;
-
-    double cen = dims.z / 2;
-    for (int i = 0; i < dims.x; i++) {
+    float cen = dims.z / 2;
+    float wy, wz;
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < dims.x; i++)
         for (int j = 0; j < dims.y; j++) {
-            double y = (j - cen)/dims.y;
-            double wy = kaiserFT(W, beta, y);
+            if (twod)
+                wy = kernel.kaiserFT((j - cen) / dims.y);
+            else 
+                wy = 1.f; 
             for (int k = 0; k < dims.z; k++) {
-                double x = (k - cen)/dims.z;
-                int idx = i * dims.y * dims.z + j * dims.z + k;
-                arr[idx] /= (wy * kaiserFT(W, beta, x));
+                wz = kernel.kaiserFT((k - cen) / dims.z);
+                arr(i,j,k) /= wy * wz;
             }
         }
-    }
 }
