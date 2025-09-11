d2daeaa17f382427fb5e8ed34afd36241e2c7bec
aryabhatt <dinesharma@gmail.com>
2025-09-11 11:25:14 -0700

    calling constructor for dims_t

--- src/projection.cpp
+++ src/projection.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-#include <vector>
-
 #include "array.h"
 #include "array_ops.h"
 #include "dtypes.h"
@@ -10,24 +7,20 @@
 #include "padding.h"
 #include "polar_grid.h"
 #include "tomocam.h"
+#include <cstdint>
 
-constexpr double tol = 1.0E-15;
 constexpr double crop_factor = 0.4142135624;
 
 namespace tomocam {
 
-    template <typename T>
-    Array<T> forward(const Array<T> &image, const PolarGrid<T> &pg) {
+    template <typename T> Array<T> forward(const Array<T> &image, const PolarGrid<T> &pg) {
 
-        int nslcs = image.nslices();
-        int nrows = image.nrows();
-        int ncols = image.ncols();
-        dims_t pad_size;
+        auto nrows = image.nrows();
+        auto ncols = image.ncols();
+        dims_t pad_size{2 * (static_cast<uint64_t>(nrows * crop_factor) / 2),
+                        2 * (static_cast<uint64_t>(nrows * crop_factor) / 2),
+                        2 * (static_cast<uint64_t>(ncols * crop_factor) / 2)};
 
-        // pad 3d aray
-        pad_size.x = 2 * (static_cast<int>(nrows * crop_factor) / 2);
-        pad_size.y = 2 * (static_cast<int>(nrows * crop_factor) / 2);
-        pad_size.z = 2 * (static_cast<int>(ncols * crop_factor) / 2);
         auto F = pad3d(image, pad_size, PadType::SYMMETRIC);
 
         // cast double array to complex
@@ -43,31 +36,24 @@ namespace tomocam {
         C = fft::fftshift(C, fft::Axes::two);
 
         // crop projection
-        pad_size.x = 0;
-        C = crop2d(C, pad_size, PadType::SYMMETRIC);
-        return real<T>(C);
+        dims_t crop_size{0, pad_size.y(), pad_size.z()};
+        C = crop2d(C, crop_size, PadType::SYMMETRIC);
+        return to_real<T>(C);
     }
 
-    template <typename T>
-    Array<T> backward(const Array<T> &proj, const PolarGrid<T> &pg) {
-
-        // set switch for double or float
-        bool is_double = std::is_same_v<T, double>;
+    template <typename T> Array<T> backward(const Array<T> &proj, const PolarGrid<T> &pg) {
 
         // dimensions
         dims_t dims = proj.dims();
-        int nprojs = dims.x;
-        int nrows = dims.y;
-        int ncols = dims.z;
+        auto nrows = dims.y();
+        auto ncols = dims.z();
 
         // cast to complex
         auto C = to_complex(proj);
 
         // pad each projection by factor of sqrt(2)
-        dims_t pad_size;
-        pad_size.x = 0;
-        pad_size.y = 2 * (static_cast<int>(nrows * crop_factor) / 2);
-        pad_size.z = 2 * (static_cast<int>(ncols * crop_factor) / 2);
+        dims_t pad_size{0, 2 * (static_cast<uint64_t>(nrows * crop_factor) / 2),
+                        2 * (static_cast<uint64_t>(ncols * crop_factor) / 2)};
         C = pad2d(C, pad_size, PadType::SYMMETRIC);
 
         // 2-D Fourier transforms
@@ -81,22 +67,17 @@ namespace tomocam {
         nufft::nufft3d1<T>(C, F, pg);
 
         // crop image
-        pad_size.x = pad_size.y;
-        F = crop3d(F, pad_size, PadType::SYMMETRIC);
-
+        dims_t crop_size{pad_size.y(), pad_size.y(), pad_size.z()};
+        F = crop3d(F, crop_size, PadType::SYMMETRIC);
         return to_real<T>(F);
     }
 
     // Explicit instantiation forward
-    template Array<float> forward(const Array<float> &,
-        const PolarGrid<float> &);
-    template Array<double> forward(const Array<double> &,
-        const PolarGrid<double> &);
+    template Array<float> forward(const Array<float> &, const PolarGrid<float> &);
+    template Array<double> forward(const Array<double> &, const PolarGrid<double> &);
 
     // Explicit instantiation backward
-    template Array<float> backward(const Array<float> &,
-        const PolarGrid<float> &);
-    template Array<double> backward(const Array<double> &,
-        const PolarGrid<double> &);
+    template Array<float> backward(const Array<float> &, const PolarGrid<float> &);
+    template Array<double> backward(const Array<double> &, const PolarGrid<double> &);
 
 } // namespace tomocam
