5366037ed5da57cfc603fddc1f0a5a601d5699ce
aryabhatt <dinesharma@gmail.com>
2025-09-11 11:24:33 -0700

    bugfix: calling constructors of x, y, z

--- src/polar_grid.h
+++ src/polar_grid.h
@@ -1,35 +1,38 @@
 
 #include "array.h"
 #include <cmath>
+#include <cstddef>
+#include <cstdint>
 #include <vector>
 
 #ifndef POLAR_GRID__H
 #define POLAR_GRID__H
 namespace tomocam {
 
-    template <typename T>
-    struct PolarGrid {
+    template <typename T> struct PolarGrid {
         size_t npts;
         Array<T> x;
         Array<T> y;
         Array<T> z;
 
-        PolarGrid(const std::vector<T> theta, int nrows, int ncols) {
-            dims_t dims = {theta.size(), nrows, ncols};
-            x(dims);
-            y(dims);
-            z(dims);
+        // array dimensions for non-uniform points
+        dims_t dims() const { return x.dims(); }
+
+        PolarGrid(const std::vector<T> theta, uint64_t nrows, uint64_t ncols) {
+            dims_t dims = dims_t{theta.size(), nrows, ncols};
+            x = Array<T>(dims);
+            y = Array<T>(dims);
+            z = Array<T>(dims);
             npts = dims.size();
 
 #pragma omp parallel for collapse(3)
-            for (int i = 0; i < dims.x; ++i) {
-                for (int j = 0; j < dims.y; ++j) {
-                    for (int k = 0; k < dims.z; ++k) {
-                        x[i, j, k] = 2 * M_PI / (dims.z - 1) * (k - dims.z / 2);
-                        y[i, j, k] = 2 * M_PI / (dims.y - 1) *
-                                     (j - dims.y / 2) * std::sin(theta[i]);
-                        z[i, j, k] = 2 * M_PI / (dims.y - 1) *
-                                     (j - dims.y / 2) * std::cos(theta[i]);
+            for (uint64_t i = 0; i < dims.x(); ++i) {
+                for (uint64_t j = 0; j < dims.y(); ++j) {
+                    for (uint64_t k = 0; k < dims.z(); ++k) {
+                        x[i, j, k] = -M_PI + i * (2 * M_PI) / static_cast<T>(dims.z() - 1);
+                        T r = -M_PI + k * (2 * M_PI) / static_cast<T>(dims.y() - 1);
+                        y[i, j, k] = r * std::cos(theta[i]);
+                        z[i, j, k] = r * std::sin(theta[i]);
                     }
                 }
             }
