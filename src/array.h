29807ceec013fac7c5fa02e5d2df40d352e714d6
aryabhatt <dinesharma@gmail.com>
2025-09-11 09:57:26 -0700

    1. dims_t is now a class not struct 2. using 64-bit unsigned integers for dimensions

--- src/array.h
+++ src/array.h
@@ -2,6 +2,7 @@
 #include <cstddef>
 #include <cstdint>
 #include <execution>
+#include <future>
 #include <memory>
 #include <string.h>
 #include <tuple>
@@ -12,43 +13,19 @@
 #define ARRAY3__H
 
 namespace tomocam {
-    template <typename T>
-    class Array {
+    template <typename T> class Array {
       private:
         dims_t dims_;
-        size_t size_;
+        uint64_t size_;
         std::unique_ptr<T[]> ptr_;
 
-        size_t flatIdx(int i, int j, int k) {
-            return (static_cast<size_t>(i) * dims_.x + static_cast<size_t>(j)) *
-                       dims_.y +
-                   static_cast<size_t>(k);
-        }
-        size_t mul(int i, int j, int k) {
-            return (static_cast<size_t>(i) * static_cast<size_t>(j) *
-                    static_cast<size_t>(k));
-        }
-
       public:
-        Array() : dims_{0, 0, 0}, size_(0), ptr_(nullptr) {}
+        Array() : dims_(0, 0, 0), size_(0), ptr_(nullptr) {}
 
-        Array(int x, int y, int z) :
-            dims_(x, y, z),
-            size_(mul(x, y, z)),
-            ptr_(std::make_unique<T[]>(mul(x, y, z))) {}
+        Array(uint64_t x, uint64_t y, uint64_t z)
+            : dims_(x, y, z), size_(dims_.size()), ptr_(std::make_unique<T[]>(size_)) {}
 
-        Array(int x, int y, int z, T v) :
-            dims_(x, y, z),
-            size_(mul(x, y, z)),
-            ptr_(std::make_unique<T[]>(mul(x, y, z))) {
-            std::fill(std::execution::par_unseq, ptr_.get(), ptr_.get() + size_,
-                v);
-        }
-
-        Array(dims_t d) :
-            dims_(d),
-            size_(mul(d.x, d.y, d.z)),
-            ptr_(std::make_unique<T[]>(mul(d.x, d.y, d.z))) {}
+        Array(dims_t d) : dims_(d), size_(dims_.size()), ptr_(std::make_unique<T[]>(size_)) {}
 
         Array(const Array &rhs) {
             dims_ = rhs.dims_;
@@ -68,21 +45,22 @@ namespace tomocam {
             return *this;
         }
 
-        Array &operator=(T v) {
-            std::fill(std::execution::par_unseq, this->begin(), this->end(), v);
-            return *this;
+        std::tuple<uint64_t, uint64_t, uint64_t> unravel_idx(uint64_t idx) {
+            return dims_.unravel_idx(idx);
         }
 
-        std::tuple<size_t, size_t, size_t> unravel_idx(size_t idx) {
-            size_t i = idx / dims_.y / dims_.z;
-            size_t j = (idx / dims_.z) % dims_.y;
-            size_t k = idx % dims_.z;
-            return std::make_tuple(i, j, k);
+        uint64_t flatIdx(uint64_t i, uint64_t j, uint64_t k) const {
+            return dims_.flat_idx(i, j, k);
         }
 
-        size_t flat_idx(size_t i, size_t j, size_t k) {
-            return (i * dims_.y * dims_.z + j * dims_.z + k);
+        uint64_t flatIdx(int i0, int j0, int k0) const {
+            uint64_t i1 = static_cast<uint64_t>(i1);
+            uint64_t j1 = static_cast<uint64_t>(j1);
+            uint64_t k1 = static_cast<uint64_t>(k1);
+            return dims_.flat_idx(i1, j1, k1);
         }
+
+        void fill(T v) { std::fill(std::execution::par_unseq, this->begin(), this->end(), v); }
 
         T *begin() { return ptr_.get(); }
         const T *begin() const { return ptr_.get(); }
@@ -90,131 +68,99 @@ namespace tomocam {
         const T *end() const { return ptr_.get() + size_; }
 
         dims_t dims() const { return dims_; }
-        size_t size() const { return size_; }
-        int nslices() const { return dims_.x; }
-        int nrows() const { return dims_.y; }
-        int ncols() const { return dims_.z; }
+        uint64_t size() const { return size_; }
+        uint64_t nslices() const { return dims_.x(); }
+        uint64_t nrows() const { return dims_.y(); }
+        uint64_t ncols() const { return dims_.z(); }
 
         // indexing
-        T &operator[](int i) { return ptr_[i]; }
-        T operator[](int i) const { return ptr_[i]; }
+        T &operator[](uint64_t i) { return ptr_[i]; }
+        T operator[](uint64_t i) const { return ptr_[i]; }
 
-        T &operator[](int i, int j, int k) { return ptr_[flatIdx(i, j, k)]; }
-        T operator()(int i, int j, int k) const {
-            return ptr_[flatIdx(i, j, k)];
-        }
+        T &operator[](uint64_t i, uint64_t j, uint64_t k) { return ptr_[flatIdx(i, j, k)]; }
+        T operator[](uint64_t i, uint64_t j, uint64_t k) const { return ptr_[flatIdx(i, j, k)]; }
 
-        T &operator[](dims_t i) { return ptr_[flatIdx(i.x, i.y, i.z)]; }
-        T operator[](dims_t i) const { return ptr_[flatIdx(i.x, i.y, i.z)]; }
+        T &operator[](dims_t i) { return ptr_[flatIdx(i.x(), i.y(), i.z())]; }
+        const T &operator[](dims_t i) const { return ptr_[flatIdx(i.x(), i.y(), i.z())]; }
 
         // multiplication operators
         Array<T> &operator*=(T v) {
-            std::transform(std::execution::par_unseq, this->begin(),
-                this->end(), ptr_.get(), [v](T x) { return x * v; });
+            std::transform(std::execution::par_unseq, this->begin(), this->end(), ptr_.get(),
+                           [v](T x) { return x * v; });
             return *this;
         }
 
         // division
         Array<T> &operator/=(T v) {
             if (v == 0) {
-                throw std::runtime_error(
-                    "Division by zero in Array<T>::operator/=");
+                throw std::runtime_error("Division by zero in Array<T>::operator/=");
             }
-            std::transform(std::execution::par_unseq, this->begin(),
-                this->end(), ptr_.get(), [v](T x) { return x / v; });
+            std::transform(std::execution::par_unseq, this->begin(), this->end(), ptr_.get(),
+                           [v](T x) { return x / v; });
             return *this;
         }
         Array<T> &operator/=(const Array<T> &v) {
-            std::transform(std::execution::par_unseq, this->begin(),
-                this->end(), v.ptr_.get(), ptr_.get(), [](T x, T y) {
-                    if (y == 0) {
-                        throw std::runtime_error(
-                            "Division by zero in Array<T>::operator/=");
-                    }
-                    return x / y;
-                });
+            std::transform(std::execution::par_unseq, this->begin(), this->end(), v.ptr_.get(),
+                           ptr_.get(), [](T x, T y) {
+                               if (y == 0) {
+                                   throw std::runtime_error(
+                                       "Division by zero in Array<T>::operator/=");
+                               }
+                               return x / y;
+                           });
             return *this;
         }
 
         // addition
         Array<T> &operator+=(T v) {
-            std::transform(std::execution::par_unseq, this->begin(),
-                this->end(), ptr_.get(), [v](T x) { return x + v; });
+            std::transform(std::execution::par_unseq, this->begin(), this->end(), ptr_.get(),
+                           [v](T x) { return x + v; });
             return *this;
         }
         Array<T> &operator+=(const Array<T> &v) {
-            std::transform(std::execution::par_unseq, this->begin(),
-                this->end(), v.ptr_.get(), ptr_.get(), std::plus<T>());
+            std::transform(std::execution::par_unseq, this->begin(), this->end(), v.ptr_.get(),
+                           ptr_.get(), std::plus<T>());
             return *this;
         }
 
         // subtraction
         Array<T> &operator-=(T v) {
-            std::transform(std::execution::par_unseq, this->begin(),
-                this->end(), ptr_.get(), [v](T x) { return x - v; });
+            std::transform(std::execution::par_unseq, this->begin(), this->end(), ptr_.get(),
+                           [v](T x) { return x - v; });
             return *this;
         }
         Array<T> &operator-=(const Array<T> &v) {
-            std::transform(std::execution::par_unseq, this->begin(),
-                this->end(), v.ptr_.get(), ptr_.get(), std::minus<T>());
+            std::transform(std::execution::par_unseq, this->begin(), this->end(), v.ptr_.get(),
+                           ptr_.get(), std::minus<T>());
             return *this;
         }
     };
 
     // multiplications
-    template <typename T>
-    Array<T> &operator*(Array<T> a, const Array<T> &b) {
-        return a *= b;
-    }
-    template <typename T>
-    Array<T> &operator*(T a, Array<T> b) {
-        return b *= a;
-    }
-    template <typename T>
-    Array<T> &operator*(Array<T> b, T a) {
-        return b *= a;
-    }
+    template <typename T> Array<T> &operator*(Array<T> a, const Array<T> &b) { return a *= b; }
+    template <typename T> Array<T> &operator*(T a, Array<T> b) { return b *= a; }
+    template <typename T> Array<T> &operator*(Array<T> b, T a) { return b *= a; }
     //  addtitions
-    template <typename T>
-    Array<T> &operator+(Array<T> a, const Array<T> &b) {
-        return a += b;
-    }
-    template <typename T>
-    Array<T> &operator+(T a, Array<T> b) {
-        return b += a;
-    }
-    template <typename T>
-    Array<T> &operator+(Array<T> b, T a) {
-        return b += a;
-    }
+    template <typename T> Array<T> &operator+(Array<T> a, const Array<T> &b) { return a += b; }
+    template <typename T> Array<T> &operator+(T a, Array<T> b) { return b += a; }
+    template <typename T> Array<T> &operator+(Array<T> b, T a) { return b += a; }
     // subtractions
-    template <typename T>
-    Array<T> &operator-(Array<T> a, const Array<T> &b) {
-        return a -= b;
-    }
+    template <typename T> Array<T> &operator-(Array<T> a, const Array<T> &b) { return a -= b; }
 
-    template <typename T>
-    Array<T> operator-(double a, Array<T> b) {
+    template <typename T> Array<T> operator-(double a, Array<T> b) {
         Array<T> rv(b.dims());
-        std::transform(std::execution::par_unseq, b.begin(), b.end(),
-            rv.begin(), [a](T x) { return a - x; });
+        std::transform(std::execution::par_unseq, b.begin(), b.end(), rv.begin(),
+                       [a](T x) { return a - x; });
         return rv;
     }
 
     // divisions
-    template <typename T>
-    Array<T> &operator/(Array<T> a, const Array<T> &b) {
-        return a /= b;
-    }
-    template <typename T>
-    Array<T> &operator/(Array<T> a, T b) {
-        return a /= b;
-    }
-    template <typename T>
-    Array<T> operator/(double a, Array<T> b) {
+    template <typename T> Array<T> &operator/(Array<T> a, const Array<T> &b) { return a /= b; }
+    template <typename T> Array<T> &operator/(Array<T> a, T b) { return a /= b; }
+    template <typename T> Array<T> operator/(double a, Array<T> b) {
         Array<T> rv(b.dims());
-        std::transform(std::execution::par_unseq, b.begin(), b.end(),
-            rv.begin(), [a](T x) { return a / x; });
+        std::transform(std::execution::par_unseq, b.begin(), b.end(), rv.begin(),
+                       [a](T x) { return a / x; });
         return rv;
     }
 
--- src/dtypes.h
+++ src/dtypes.h
@@ -1,34 +1,44 @@
-#include <memory>
+#include <cstdint>
 #include <stdexcept>
+#include <tuple>
 
 #ifndef DTYPES__H
 #define DTYPES__H
 
 namespace tomocam {
 
-    struct dims_t {
-        size_t x;
-        size_t y;
-        size_t z;
-        size_t size() const { return x * y * z; }
+    class dims_t {
+        uint64_t n1;
+        uint64_t n2;
+        uint64_t n3;
 
-        dims_t operator+(const dims_t &v) {
-            return {this->x + v.x, this->y + v.y, this->z + v.z};
+      public:
+        explicit dims_t(uint64_t a, uint64_t b, uint64_t c) : n1(a), n2(b), n3(c) {}
+
+        std::tuple<uint64_t, uint64_t, uint64_t> unravel_idx(uint64_t idx) const {
+            uint64_t i = idx / n2 / n3;
+            uint64_t j = (idx / n3) % n2;
+            uint64_t k = idx % n3;
+            return std::make_tuple(i, j, k);
         }
 
-        dims_t operator-(const dims_t &v) {
-            return {this->x - v.x, this->y - v.y, this->z - v.z};
-        }
+        uint64_t size() const { return (n1 * n2 * n3); }
 
-        dims_t operator*(int v) {
-            return {this->x * v, this->y * v, this->z * v};
+        uint64_t flat_idx(uint64_t i, uint64_t j, uint64_t k) const {
+            return ((i * n2 + j) * n3 + k);
         }
+        uint64_t x() const { return n1; }
+        uint64_t y() const { return n2; }
+        uint64_t z() const { return n3; }
 
+        dims_t operator+(const dims_t &v) { return dims_t(n1 + v.n1, n2 + v.n2, n3 + v.n3); }
+        dims_t operator-(const dims_t &v) { return dims_t(n1 - v.n1, n2 - v.n2, n3 - v.n3); }
+        dims_t operator*(int v) { return dims_t(n1 * v, n2 * v, n3 * v); }
         dims_t operator/(int v) {
-            if (v == 0) std::runtime_error("divide by zeros");
-            return {this->x / v, this->y / v, this->z / v};
+            if (v == 0)
+                throw std::runtime_error("divide by zeros");
+            return dims_t(n1 / v, n2 / v, n3 / v);
         }
     };
-
 } // namespace tomocam
 #endif // DTYPES__H
