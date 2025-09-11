e82d9208945cb26859a0e83add10824aec9a84ab
aryabhatt <dinesharma@gmail.com>
2025-09-11 11:21:17 -0700

    1. appropriate changes to use dims_t as a class 2. using uint64_t for sizes instead of int

--- src/padding.h
+++ src/padding.h
@@ -1,5 +1,7 @@
 
 #include "array.h"
+#include "dtypes.h"
+#include <cstdint>
 
 #ifndef PADDING__H
 #define PADDING__H
@@ -7,13 +9,14 @@
 namespace tomocam {
     enum class PadType { LEFT, RIGHT, SYMMETRIC };
 
-    template <typename T>
-    Array<T> pad1d(const Array<T> &arr, int pad_size, PadType pad_type) {
+    template <typename T> Array<T> pad1d(const Array<T> &arr, uint64_t pad_size, PadType pad_type) {
 
-        if (pad_size == 0) { return arr; }
+        if (pad_size == 0) {
+            return arr;
+        }
 
         auto dims = arr.dims();
-        Array<T> arr2(dims.x, dims.y + pad_size);
+        Array<T> arr2(dims.x(), dims.y(), dims.z() + pad_size);
 
         // assume symmetric padding by default
         int d = pad_size / 2;
@@ -23,18 +26,21 @@ namespace tomocam {
             d = pad_size;
         }
 
-        for (int i = 0; i < dims.x; i++) {
-            for (int j = 0; j < dims.y; j++) new_arr(i, j + d) = arr(i, j);
-        }
-        return new_arr;
+        for (int i = 0; i < dims.x(); i++)
+            for (int j = 0; j < dims.y(); j++)
+                for (int k = 0; k < dims.z(); k++)
+                    arr2[i, j, k + d] = arr[i, j, k];
+        return arr2;
     }
 
     template <typename T>
-    Array<T> crop1d(const Array<T> &arr, int crop_size, PadType pad_type) {
+    Array<T> crop1d(const Array<T> &arr, uint64_t crop_size, PadType pad_type) {
 
-        if (crop_size == 0) { return arr; }
-        auto dims = arr.dims();
-        Array<T> new_arr(dims.x, dims.y - crop_size);
+        if (crop_size == 0) {
+            return arr;
+        }
+        auto dims = arr.dims() - dims_t{0, 0, crop_size};
+        Array<T> arr2(dims);
 
         // assume symmetric padding by default
         int d = crop_size / 2;
@@ -44,93 +50,110 @@ namespace tomocam {
             d = crop_size;
         }
 
-        for (int i = 0; i < dims.x; i++) {
-            for (int j = 0; j < dims.y - crop_size; j++)
-                new_arr(i, j) = arr(i, j + d);
-        }
-        return new_arr;
+        for (int i = 0; i < dims.x(); i++)
+            for (int j = 0; j < dims.y(); j++)
+                for (int k = 0; k < dims.z(); k++)
+                    arr2[i, j, k] = arr[i, j, k + d];
+        return arr2;
     }
 
-    template <typename T>
-    Array<T> pad2d(const Array<T> &arr, dims_t pad_size, PadType pad_type) {
+    template <typename T> Array<T> pad2d(const Array<T> &arr, dims_t pad_size, PadType pad_type) {
 
-        if ((pad_size.y == 0) && (pad_size.z == 0)) { return arr; }
+        if ((pad_size.y() == 0) && (pad_size.z() == 0)) {
+            return arr;
+        }
 
+        // create and initialize return array
         auto dims = arr.dims();
-        auto new_dims = {dims.x, dims.y + pad_size.y, dims.z + pad_size.z};
-        Array<T> new_arr(new_dims);
-        new_arr = 0;
+        dims_t new_dims = dims + pad_size;
+        Array<T> arr2(new_dims);
+        arr2.fill(static_cast<T>(0));
 
         // assume symmetric padding by default
-        dims_t d = {pad_size.x, pad_size.y / 2, pad_size.z / 2};
+        dims_t d = pad_size / 2;
         if (pad_type == PadType::RIGHT) {
-            d = {0, 0, 0};
+            d = dims_t(0, 0, 0);
         } else if (pad_type == PadType::LEFT) {
             d = pad_size;
         }
 
-        for (int i = 0; i < dims.x; i++)
-            for (int j = 0; j < dims.y; j++)
-                for (int k = 0; k < dims.z; k++)
-                    new_arr[i, j + d.y, k + d.z] = arr[i, j, k];
-        return new_arr;
+        for (int i = 0; i < dims.x(); i++)
+            for (int j = 0; j < dims.y(); j++)
+                for (int k = 0; k < dims.z(); k++)
+                    arr2[i, j + d.y(), k + d.z()] = arr[i, j, k];
+        return arr2;
     }
 
-    template <typename T>
-    Array<T> crop2d(const Array<T> &arr, dims_t crop_size, PadType pad_type) {
-        if ((crop_size.y == 0) && (crop_size.z == 0)) { return arr; }
+    template <typename T> Array<T> crop2d(const Array<T> &arr, dims_t crop_size, PadType pad_type) {
+
+        if ((crop_size.y() == 0) && (crop_size.z() == 0)) {
+            return arr;
+        }
 
         auto dims = arr.dims();
-        dims_t new_dims = {dims.x, dims.y - crop_size.y, dims.z - crop_size.z};
-        Array<T> new_arr(new_dims);
+        dims_t new_dims = dims - crop_size;
+        Array<T> arr2(new_dims);
 
         // assume symmetric padding by default
-        dims_t d = {crop_size.x, crop_size.y / 2, crop_size.z / 2};
+        dims_t d = crop_size / 2;
         if (pad_type == PadType::RIGHT) {
-            d = {0, 0, 0};
+            d = dims_t(0, 0, 0);
         } else if (pad_type == PadType::LEFT) {
             d = crop_size;
         }
 
-        for (int i = 0; i < new_dims.x; i++) {
-            for (int j = 0; j < new_dims.y; j++)
-                for (int k = 0; k < new_dims.z;)
-                    new_arr[i, j, k] = arr[i, j + d.y, j + d.z];
+        for (int i = 0; i < new_dims.x(); i++) {
+            for (int j = 0; j < new_dims.y(); j++)
+                for (int k = 0; k < new_dims.z(); k++)
+                    arr2[i, j, k] = arr[i, j + d.y(), k + d.z()];
         }
-        return new_arr;
+        return arr2;
     }
 
-    template <typename T>
-    Array<T> pad3d(const Array<T> &arr, dims_t pad_size, PadType pad_type) {
+    template <typename T> Array<T> pad3d(const Array<T> &arr, dims_t pad_size, PadType pad_type) {
 
-        if ((pad_size.x == 0) && (pad_size.y == 0) && (pad_size.z == 0))
+        if ((pad_size.x() == 0) && (pad_size.y() == 0) && (pad_size.z() == 0))
             return arr;
 
         auto dims = arr.dims();
-        dims_t new_dims = { dims.x - pad_size.x }
+        dims_t new_dims = dims + pad_size;
+        Array<T> arr2(new_dims);
+
+        // assume symmetric padding
+        dims_t d = pad_size / 2;
+        if (pad_type == PadType::RIGHT) {
+            d = dims_t(0, 0, 0);
+        } else if (pad_type == PadType::LEFT) {
+            d = pad_size;
+        }
+
+        for (int i = 0; i < dims.x(); i++)
+            for (int j = 0; j < dims.y(); j++)
+                for (int k = 0; k < dims.z(); k++)
+                    arr2[i + d.x(), j + d.y(), k + d.z()] = arr[i, j, k];
+        return arr2;
     }
 
-    template <typename T>
-    Array<T> crop3d(const Array<T> &arr, dims_t crop_size, PadType pad_type) {
+    template <typename T> Array<T> crop3d(const Array<T> &arr, dims_t crop_size, PadType pad_type) {
 
-        if ((crop_size.x == 0) && (crop_size.y == 0) && (crop_size.z == 0))
+        if ((crop_size.x() == 0) && (crop_size.y() == 0) && (crop_size.z() == 0))
             return arr;
 
         auto dims = arr.dims();
-        dims_t new_dims = {dims.x - crop_size.x, dims.y - crop_size.y,
-            dims.z - crop_size.z};
-        Array<T> new_arr(new_dims);
+        dims_t new_dims = dims - crop_size;
+        Array<T> arr2(new_dims);
 
-        dims_t d = {crop_size.x / 2, crop_size.y / 2, crop_size.z / 2};
-        if (pad_type == PadType::LEFT) d = {0, 0, 0};
+        dims_t d = crop_size / 2;
+        if (pad_type == PadType::LEFT)
+            d = dims_t(0, 0, 0);
         else if (pad_type == PadType::RIGHT)
             d = crop_size;
 
-        for (int i = 0; i < new_dims.x; i++) {
-            for (int j = 0; j < new_dims.y; j++)
-                for (int k = 0; k < new_dims.z; k++)
-                    new_arr[i, j, k] = arr[i + d.x, j + d.y, k + d.z];
-        }
+        for (int i = 0; i < new_dims.x(); i++)
+            for (int j = 0; j < new_dims.y(); j++)
+                for (int k = 0; k < new_dims.z(); k++)
+                    arr2[i, j, k] = arr[i + d.x(), j + d.y(), k + d.z()];
+        return arr2;
     }
 } // namespace tomocam
 #endif // PADDING__H
