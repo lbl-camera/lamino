92eba7a45f17e91ef378e8ed1d4987e607bb384f
aryabhatt <dinesharma@gmail.com>
2025-09-11 11:28:00 -0700

    removed code that hasn't been adapted for 3d

--- src/results.cpp
+++ src/results.cpp
@@ -3,37 +3,14 @@
 
 #include "array.h"
 
-double calc_error(const Array<double> &array, const Array<double> &data) {
-    double err = 0.f;
+namespace tomocam {
 
-    for (int i = 0; i < array.size(); i++)
-        err += pow(array[i] - data[i], 2);
-    return std::sqrt(err);
-}
+    template <typename T>
+    T calc_error(const Array<T> &array, const Array<T> &data) {
 
-void write_output(char * filename, char *buffer, size_t bytes) {
-    std::ofstream out(filename, std::ios::out | std::ios::binary);
-    out.write(buffer, bytes);
-    out.close();
-}
-
-void ramp_filter(Array<complex<double>> & array, double fmax) {
-
-    dim2_t dims = array.dims();
-    double cen = static_cast<double>(dims.y) / 2.f;
-    double a = static_cast<double>(dims.y) / M_PI;
-
-    int jmin = static_cast<int>((1. - fmax) * dims.y) / 2;
-    int jmax = dims.y - jmin;
-    for (int i = 0; i < dims.x; i++) {
-        for (int j = 0; j < dims.y; j++) {
-            if (j < jmin) array(i, j) = 0.f;
-            else if (j > jmax) array(i, j) = 0.f;
-            else {
-                double t = static_cast<double>(j - cen) / static_cast<double>(dims.y);
-                double w = a * std::abs(std::sin(t * M_PI));
-                array(i, j) *= w;
-            }
-        }
+        T err = 0.f;
+        for (int i = 0; i < array.size(); i++)
+            err += std::pow(array[i] - data[i], 2);
+        return std::sqrt(err);
     }
 }
--- test/forward.cpp
+++ test/forward.cpp
@@ -1,30 +1,63 @@
 #include <fstream>
 #include <iostream>
+#include <nlohmann/json.hpp>
+#include <ostream>
+#include <string>
+using json = nlohmann::json;
 
+#include "tiff.h"
+#include "timer.h"
 #include "tomocam.h"
-#include "array.h"
-#include "adam.h"
 
-const int MAX_ITERS = 1;
-const int num_angles = 400;
-const int num_pixels = 400;
-const char * FILENAME = "/home/dkumar/data/shepp_logan/shepp400.bin";
+void usage(char **argv) {
+    std::cout << "Usage: " << argv[0] << " JSON input configuration" << std::endl;
+    exit(0);
+}
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+
+    // sanity check
+    if (argc < 2)
+        usage(argv);
 
     // read data
-    Array<float> image(num_pixels, num_pixels);
-    image.fromfile(FILENAME);
+    std::ifstream json_file(argv[1]);
+    if (!json_file.is_open()) {
+        std::cerr << "error: can't open JSON file: " << argv[1] << std::endl;
+        return 1;
+    }
 
-    Array <float> angles(1, num_angles);
-    for (int i = 0; i < num_angles; i++)
-        angles[i] = i * M_PI / (num_angles - 1);
+    // get input data
+    json config = json::parse(json_file);
+    std::string filename = config["filename"];
+    std::string output = config["output"];
 
-    // radon transform
-    float center = 200;
-	auto sino = forward(image, angles, center);
-		
-    sino.tofile("output.bin");
+    tomocam::Timer t0;
+    t0.start();
+    auto sample = tomocam::tiff::read(filename);
+    t0.stop();
+    std::cerr << "Time to read data: " << t0.ms() << std::endl;
+
+    std::vector<float> theta(141, 0);
+    for (int i = 0; i < theta.size(); i++) {
+        int deg = -1 * (theta.size() / 2) + i;
+        theta[i] = deg * M_PI / 180.f;
+    }
+
+    // create a polar grid
+    t0.start();
+    tomocam::PolarGrid<float> pgrid(theta, sample.nrows(), sample.ncols());
+    t0.stop();
+    std::cerr << "Time to build a polar grid: " << t0.ms() << std::endl;
+
+    // do the forward projection
+    t0.start();
+    auto proj = tomocam::forward(sample, pgrid);
+    t0.stop();
+    std::cerr << "time to do forward projection: " << t0.ms() << std::endl;
+
+    // save data to tiff-stack
+    tomocam::tiff::write(output, proj);
+
     return 0;
 }
