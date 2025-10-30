1ee62c2c051c70f047bddb5971a9962ad1764853
Dinesh Kumar <dkumar@lbl.gov>
2025-10-30 13:18:45 -0700

    Refactor dims_t to use size_t instead of uint64_t for dimension variables and related methods.

--- include/dtypes.h
+++ include/dtypes.h
@@ -4,41 +4,40 @@
 #include <tuple>
 
 #ifndef DTYPES__H
-#define DTYPES__H
+    #define DTYPES__H
 
 namespace tomocam {
 
-    class dims_t {
-        uint64_t n1;
-        uint64_t n2;
-        uint64_t n3;
+    struct dims_t {
+        size_t n1;
+        size_t n2;
+        size_t n3;
 
       public:
         dims_t() : n1(0), n2(0), n3() {}
-        dims_t(uint64_t a, uint64_t b, uint64_t c) : n1(a), n2(b), n3(c) {}
+        dims_t(size_t a, size_t b, size_t c) : n1(a), n2(b), n3(c) {}
 
-        [[nodiscard]] std::tuple<uint64_t, uint64_t, uint64_t> unravel_idx(
-            uint64_t idx) const {
-            uint64_t i = idx / n2 / n3;
-            uint64_t j = (idx / n3) % n2;
-            uint64_t k = idx % n3;
+        [[nodiscard]] std::tuple<size_t, size_t, size_t> unravel_idx(
+            size_t idx) const {
+            size_t i = idx / n2 / n3;
+            size_t j = (idx / n3) % n2;
+            size_t k = idx % n3;
             return std::make_tuple(i, j, k);
         }
 
         // change individual dims (needed in padding)
-        void set_x(uint64_t v) { n1 = v; }
-        void set_y(uint64_t v) { n2 = v; }
-        void set_z(uint64_t v) { n3 = v; }
+        void set_x(size_t v) { n1 = v; }
+        void set_y(size_t v) { n2 = v; }
+        void set_z(size_t v) { n3 = v; }
 
-        [[nodiscard]] uint64_t size() const { return (n1 * n2 * n3); }
+        [[nodiscard]] size_t size() const { return (n1 * n2 * n3); }
 
-        [[nodiscard]] uint64_t flat_idx(uint64_t i, uint64_t j,
-            uint64_t k) const {
+        [[nodiscard]] size_t flat_idx(size_t i, size_t j, size_t k) const {
             return (((i * n2 + j) * n3) + k);
         }
-        [[nodiscard]] uint64_t x() const { return n1; }
-        [[nodiscard]] uint64_t y() const { return n2; }
-        [[nodiscard]] uint64_t z() const { return n3; }
+        [[nodiscard]] size_t x() const { return n1; }
+        [[nodiscard]] size_t y() const { return n2; }
+        [[nodiscard]] size_t z() const { return n3; }
 
         dims_t operator+(const dims_t &v) const {
             return {n1 + v.n1, n2 + v.n2, n3 + v.n3};
