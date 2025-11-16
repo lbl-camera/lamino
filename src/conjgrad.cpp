e3cc46957224389f726ff64a4d36f7a41953ca62
Dinesh Kumar <dkumar@lbl.gov>
2025-11-16 14:24:36 -0800

    - Added copyright and license notice at the top of conjgrad.cpp. - Updated template instantiations accordingly. - Included "optimize.h" for Function type definition.

--- src/conjgrad.cpp
+++ src/conjgrad.cpp
@@ -1,3 +1,24 @@
+// clang-format off
+/* -------------------------------------------------------------------------------
+ * Tomocam Copyright (c) 2018
+ *
+ * The Regents of the University of California, through Lawrence Berkeley
+ * National Laboratory (subject to receipt of any required approvals from the
+ * U.S. Dept. of Energy). All rights reserved.
+ *
+ * If you have questions about your rights to use or distribute this software,
+ * please contact Berkeley Lab's Innovation & Partnerships Office at
+ * IPO@lbl.gov.
+ *
+ * NOTICE. This Software was developed under funding from the U.S. Department of
+ * Energy and the U.S. Government consequently retains certain rights. As such,
+ * the U.S. Government has been granted for itself and others acting on its
+ * behalf a paid-up, nonexclusive, irrevocable, worldwide license in the Software
+ * to reproduce, distribute copies to the public, prepare derivative works, and
+ * perform publicly and display publicly, and to permit other to do so.
+ *---------------------------------------------------------------------------------
+ */
+ //clang-format on
 #include <execution>
 #include <format>
 #include <functional>
@@ -5,12 +26,13 @@
 
 #include "array.h"
 #include "array_ops.h"
+#include "optimize.h"
 
 namespace tomocam::opt {
 
     template <typename T>
-    Array<T> cgsolver(const std::function<Array<T>(const Array<T> &)> &A,
-        const Array<T> &yT, size_t max_iter, T tol) {
+    Array<T> cgsolver(const Function<T> &A, const Array<T> &yT, size_t max_iter,
+                      T tol) {
 
         // initialize
         auto x = yT.clone();
@@ -41,11 +63,9 @@ namespace tomocam::opt {
     }
 
     // template instantiations
-    template Array<float> cgsolver<float>(
-        const std::function<Array<float>(const Array<float> &)> &, 
-        const Array<float> &, size_t, float);
-    template Array<double> cgsolver<double>(
-        const std::function<Array<double>(const Array<double> &)> &,
-        const Array<double> &, size_t, double);
+    template Array<float> cgsolver<float>(const Function<float> &,
+                                          const Array<float> &, size_t, float);
+    template Array<double> cgsolver<double>(const Function<double> &,
+                                            const Array<double> &, size_t, double);
 
 } // namespace tomocam::opt
