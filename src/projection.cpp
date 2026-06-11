d9b78bacb8baba1b5837c5df5507e107a7316312
Dinesh Kumar <dkumar@lbl.gov>
2026-06-11 15:49:23 -0700

    Remove normalization factor from FFT3 operation

--- src/projection.cpp
+++ src/projection.cpp
@@ -94,6 +94,7 @@ namespace tomocam {
 
         // cast to complex
         auto c_cmplx = array::to_complex(proj);
+        // T scale = static_cast<T>(proj.nrows() * proj.ncols());
 
         // 2-D Fourier transforms
         c_cmplx = fft::fftshift2(c_cmplx);
@@ -103,9 +104,6 @@ namespace tomocam {
         // nufft - each component separately
         std::array<Array<T>, 3> m_components;
         using complex_t = std::complex<T>;
-
-        // normalization factor
-        T scale = static_cast<T>(pg.size() / pg.nprojs());
 
         for (size_t i = 0; i < 3; ++i) {
             auto c_cmplx_copy = c_cmplx.clone();
@@ -121,7 +119,7 @@ namespace tomocam {
             // apply NUFFT for this component
             Array<complex_t> m_cmplx(recon_dims);
             nufft::nufft3d1<T>(c_cmplx_copy, m_cmplx, pg);
-            m_components[i] = std::move(array::to_real<T>(m_cmplx) / scale);
+            m_components[i] = std::move(array::to_real<T>(m_cmplx));
         }
 
         return m_components;
